694a6995a09e584497bfe750bd700f7d118e2ad5
Philip Skaria <philipskaria04@gmail.com>
2025-09-09 02:19:59 +0530

    videocard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import {VideoCard} from "../components/VideoCard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,14 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div>
-      hi there
+      <VideoCard
+      thumbnail={"thumbnail.png"}
+      title={"random youtube video"}
+      channel={"channel.png"}
+      author={"Philip Skaria"}
+      views={"200k"}
+      timestamp={"3 days ago"}    
+      ></VideoCard>
     </div>
   );
 }
